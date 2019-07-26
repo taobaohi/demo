e765aae6f700c68f9ad8d0611f705756b4151dab
lesheng <taobaohi@foxmail.com>
2019-07-26 18:22:02 +0800

    swagger

--- DotnetDemo/src/DotnetTemplate/Startup.cs
+++ DotnetDemo/src/DotnetTemplate/Startup.cs
@@ -3,24 +3,25 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace servicedemo
 {
     using CSRedis;
-    using Microsoft.Extensions.Caching.Distributed;
+    using Microsoft.OpenApi.Models;
     using middleware;
+    using System;
+    using System.IO;
+    using System.Reflection;
+
     public class Startup
     {
         public IConfiguration Configuration { get; }
+        
         // 构造函数注入
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
-            // 初始化RedisHelper
-            var residConn = Configuration["ConnectionStrings:RedisConn"].ToString();
-            RedisHelper.Initialization(new CSRedisClient(residConn));
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -34,10 +35,19 @@ namespace servicedemo
                 //}
              )
             .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
             // 配置文件映射
             services.Configure<AppSettings>(Configuration);
 
-            #region RedisHelper
+            #region service injecting
+            services.AddTransient<services.User, services.UserImpl>();
+            #endregion
+
+            #region redis helper,MS Distributed Cache
+            // 初始化RedisHelper
+            //var residConn = Configuration["ConnectionStrings:RedisConn"].ToString();
+            //RedisHelper.Initialization(new CSRedisClient(residConn));
+
             var csredis = new CSRedisClient(Configuration.GetConnectionString("RedisConn"));
             //初始化 RedisHelper
             RedisHelper.Initialization(csredis);
@@ -45,9 +55,36 @@ namespace servicedemo
             services.AddSingleton<IDistributedCache>(new Microsoft.Extensions.Caching.Redis.CSRedisCache(RedisHelper.Instance));
             #endregion
 
-            #region service 注入
-            services.AddTransient<services.User, services.UserImpl>();
+            #region Register the Swagger generator, defining 1 or more Swagger documents
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
             #endregion
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,9 +101,22 @@ namespace servicedemo
             //}
             //app.UseHttpsRedirection();
 
-            // 中间件统一错误处理
+            #region swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            #endregion
+
+            #region gloabl exception
             //app.UseGloablExceptionMiddleware();
             app.UseMiddleware<ExceptionMiddleware>();
+            #endregion
+
             app.UseMvc();
         }
     }
